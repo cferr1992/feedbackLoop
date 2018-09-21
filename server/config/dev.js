0880ebac3a2344100cd9e7778c2e500b69dc85a9
Charles Ferringer <charlesferringer@yahoo.com>
2018-09-21 14:15:38 -0400

    Commit before heroku deploy.

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '138383562161-0uu1ku0josqe6hmqus546tmv0521bpo0.apps.googleusercontent.com',
+    googleClientSecret: 'veOogLBJCYL1SY7dJzo6S06a',
+    mongoURI: 'mongodb://localhost/feedback-loop',
+    cookieKey: 'hijklmnop'
+  }
